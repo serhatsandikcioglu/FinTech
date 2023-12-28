0466741e59836b93e8754cf16e6fbe34b45e7875
Serhat <serhat16_@hotmail.com>
2023-12-28 09:21:42 +0300

    Add constants.

---
+++ FinTech.Shared/Constans/AccountConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTech.Shared.Constans
+{
+    public static class AccountConstants
+    {
+        public const decimal MinimumInitialBalance = 5000;
+    }
+}
---
+++ FinTech.Shared/Constans/MoneyTransferConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace FinTech.Shared.Constans
+{
+    public class MoneyTransferConstants
+    {
+        public const decimal DailyTransferLimit = 50000;
+        public const decimal PerTransactionTransferLimit = 30000;
+    }
+}
