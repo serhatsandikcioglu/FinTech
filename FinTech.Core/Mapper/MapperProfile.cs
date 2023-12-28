0c172600581f2df411d554be7eccc3a824a3baf5
Serhat <serhat16_@hotmail.com>
2023-12-28 09:25:20 +0300

    Add mapper.

---
+++ FinTech.Core/Mapper/MapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FinTech.Core.DTOs;
+using FinTech.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTech.Core.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile() 
+        {
+            CreateMap<Account , AccountDTO>().ReverseMap();
+            CreateMap<Account , AccountCreateDTO>().ReverseMap();
+
+            CreateMap<AccountActivity,AccountActivityDTO>().ReverseMap();
+            CreateMap<AccountActivity,AccountActivityCreateDTO>().ReverseMap();
+
+            CreateMap<MoneyTransfer,MoneyTransferCreateDTO>().ReverseMap();
+        }
+    }
+}
