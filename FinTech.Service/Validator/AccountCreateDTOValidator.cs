fd01d3cb311e009443e3173885731735958a4e66
Serhat <serhat16_@hotmail.com>
2023-12-28 09:21:54 +0300

    Add validator.

---
+++ FinTech.Service/Validator/AccountCreateDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FinTech.Core.DTOs;
+using FinTech.Shared.Constans;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTech.Service.Validator
+{
+    public class AccountCreateDTOValidator : AbstractValidator<AccountCreateDTO>
+    {
+        public AccountCreateDTOValidator()
+        {
+            RuleFor(x=>x.Balance).NotEmpty().GreaterThanOrEqualTo(AccountConstants.MinimumInitialBalance).WithMessage($"Initial balance must be at least {AccountConstants.MinimumInitialBalance}.");
+        }
+    }
+}
---
+++ FinTech.Service/Validator/BalanceUpdateDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FinTech.Core.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTech.Service.Validator
+{
+    public class BalanceUpdateDTOValidator : AbstractValidator<BalanceUpdateDTO>
+    {
+        public BalanceUpdateDTOValidator() 
+        {
+            RuleFor(x=>x.Balance).NotEmpty().GreaterThanOrEqualTo(0).WithMessage("Balance cannot be less than 0");
+        }
+    }
+}
